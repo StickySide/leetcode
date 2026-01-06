def2d626039972fcd69c4b8233dd021346f0c518
Jon <jnorton6@gmail.com>
2026-01-05 21:31:27 -0500

    Add future annotations import for type hinting in TreeNode class

--- problems/104-maximum-depth-of-binary-tree/solution.py
+++ problems/104-maximum-depth-of-binary-tree/solution.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val: int = 0, left: TreeNode | None = None, right: TreeNode | None = None):
